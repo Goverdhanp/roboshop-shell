8dadf5cd6303347f79c8adb5fdc242c5091c9a53
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-04-02 16:44:58 +0530

    new 1

--- shipping.sh
+++ shipping.sh
@@ -18,9 +18,9 @@ mv target/shipping-1.0.jar shipping.jar
 
 
 
-mysql -h mysql-dev.rdevops83.online -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql-dev.rdevops83.online -uroot -pRoboShop@1 < /app/db/app-user.sql 
-mysql -h mysql-dev.rdevops83.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql-dev.gvndevops12.tech -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql-dev.gvndevops12.tech -uroot -pRoboShop@1 < /app/db/app-user.sql 
+mysql -h mysql-dev.gvndevops12.tech -uroot -pRoboShop@1 < /app/db/master-data.sql
 
 systemctl daemon-reload
 systemctl enable shipping 

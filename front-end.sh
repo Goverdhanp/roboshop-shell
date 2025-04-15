0d848a08dcc5b895811f24c5747d59a029797f76
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-04-15 17:46:07 +0530

    newcc

--- front-end.sh
+++ front-end.sh
@@ -3,30 +3,33 @@ print_head(){
 
 }
 
+log_file=/tmp/roboshop.log
+rm -f $log_file
+
 print_head Disable default nginx
-dnf module disable nginx -y
+dnf module disable nginx -y $log_file
 
 print_head enable nginx 
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y $log_file
 
 print_head install nginx
-dnf install nginx -y
+dnf install nginx -y $log_file
 
 print_head copy  nginx conf
-cp nginx.conf /etc/nginx/nginx.conf
+cp nginx.conf /etc/nginx/nginx.conf $log_file
 
 
 print_head Clean olfd content
 
 print_head Download app content
 rm -rf /usr/share/nginx/html/* 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip $log_file
 
 cd /usr/share/nginx/html 
 
 print_head Extract app content
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip $log_file
 
 print_head Start nginx service
-systemctl enable nginx 
-systemctl restart nginx
+systemctl enable nginx  $log_file
+systemctl restart nginx $log_file

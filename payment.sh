bda2fc560c61c19fb33122cba545ac348d4775aa
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-04-15 16:00:51 +0530

    app_setup

--- payment.sh
+++ payment.sh
@@ -2,15 +2,6 @@ component = payment
 source common.sh
 
 
-dnf install python3 gcc python3-devel -y
-
-useradd roboshop
-
-cp payment.service /etc/systemd/system/payment.service
-
-artifact_download
-
-
-systemd_setup
+python_app_setup
 
 
--- shipping.sh
+++ shipping.sh
@@ -1,24 +1,10 @@
 
 component = shipping
 source common.sh
-
-dnf install maven -y
-
-useradd roboshop
-
-cp shipping.service  /etc/systemd/system/shipping.service
-
-artifact_download
-
-cd /app 
-mvn clean package 
-mv target/shipping-1.0.jar shipping.jar 
-
+maven_app_setup
 
 dnf install mysql -y
 
 mysql -h 172.31.14.173 -uroot -pRoboShop@1 < /app/db/schema.sql
 mysql -h 172.31.14.173 -uroot -pRoboShop@1 < /app/db/app-user.sql 
 mysql -h 172.31.14.173 -uroot -pRoboShop@1 < /app/db/master-data.sql
-
-systemd_setup

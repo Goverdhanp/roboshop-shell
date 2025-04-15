9a0f118fc1f72ece9ecc1161bcb1c0d41a091b54
Goverdhan <137044283+Goverdhanp@users.noreply.github.com>
2025-04-15 15:22:53 +0530

    n1

--- catalogue.sh
+++ catalogue.sh
@@ -1,28 +1,8 @@
 component = catalogue
 source common.sh
 
-
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-
-dnf install nodejs -y
-
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-cp catalogue.service /etc/systemd/system/catalogue.service
-
-useradd roboshop
-
-rm -rf /app
-mkdir /app 
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
-cd /app 
-unzip /tmp/catalogue.zip
-
-cd /app 
-npm install 
-
-systemd_setup
+nodejs
 
 dnf install mongodb-mongosh -y
 mongosh --host mongo-dev.gvndevops12.tech </app/db/master-data.js
--- payment.sh
+++ payment.sh
@@ -8,14 +8,7 @@ useradd roboshop
 
 cp payment.service /etc/systemd/system/payment.service
 
-rm -rf /app
-
-mkdir /app 
-
-
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip 
-cd /app 
-unzip /tmp/payment.zip
+artifact_download
 
 
 systemd_setup
